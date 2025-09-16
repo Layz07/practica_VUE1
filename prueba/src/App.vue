b58291d13f74ec6d9bb21a3a20ada746dbb96b98
Layz07 <xaviercastaneda07@gmail.com>
2025-09-16 10:37:40 -0600

    cambiarl el app.vue

--- prueba/src/App.vue
+++ prueba/src/App.vue
@@ -2,21 +2,25 @@
   <div>
     <router-view />
 
-    <Footer />
+    
     <Menu />
+    <ValorantBody />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Footer from './components/footer.vue';
 import Menu from "./components/MenuVal.vue";
+import ValorantBody from './components/ValorantBody.vue';
 
 
 export default {
   name: 'App',
   components: {
     Footer,
-    Menu
+    Menu,
+    ValorantBody
   }
 }
 </script>
